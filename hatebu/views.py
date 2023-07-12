7d5b4ced8770fdf9eab7825b55524976288eb4dd
mwatanabe_arent <m.watanabe@arent3d.com>
2023-07-12 15:52:58 +0900

    なんとなくルフィが話している感じを出す

--- hatebu/views.py
+++ hatebu/views.py
@@ -5,7 +5,7 @@ import random
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-from bs4 import BeautifulSoup # スクレイピングするため（備考欄の取得のみ）
+from bs4 import BeautifulSoup  # スクレイピングするため（備考欄の取得のみ）
 import requests
 
 # Create your views here.
@@ -15,21 +15,22 @@ from rest_framework.response import Response
 from langchain import PromptTemplate
 from llama_index import LLMPredictor, ServiceContext, PromptHelper
 from langchain import OpenAI
-from llama_index import GPTVectorStoreIndex 
+from llama_index import GPTVectorStoreIndex
 from llama_index.readers import BeautifulSoupWebReader
 
 from common.utils import make_questions
+
 
 class Topics(APIView):
     def get(self, request):
 
         hatebu_top_json = self.get_hatebu_top_json(3)
-        #print(hatebu_top_json)
-        #return JsonResponse(hatebu_top_json , safe=False)
+        # print(hatebu_top_json)
+        # return JsonResponse(hatebu_top_json , safe=False)
 
         return HttpResponse(hatebu_top_json, content_type="application/json")
 
-    def get_hatebu_top_json(self,count):
+    def get_hatebu_top_json(self, count):
         url = "https://b.hatena.ne.jp/"
         response = requests.get(url)
         html_content = response.text
@@ -46,18 +47,18 @@ class Topics(APIView):
                 # テキストとリンクを取得
                 text = a_tag.text.strip().replace('\n', '')
                 link = a_tag['href'].replace('\n', '')
-                category= a_tag['data-entry-category'].replace('\n', '')
+                category = a_tag['data-entry-category'].replace('\n', '')
 
                 entry = {
-                    "title":text,
-                    "link":link,
-                    "category":category,
+                    "title": text,
+                    "link": link,
+                    "category": category,
                 }
 
                 # a_tagの中の要素を配列に入れ直す
                 temp_a_tag.append(entry)
 
-        if count <= len(temp_a_tag) :
+        if count <= len(temp_a_tag):
             a_tag_list = random.sample(temp_a_tag, count)
         else:
             # 要素数が3未満の場合の処理
@@ -70,22 +71,22 @@ class Topics(APIView):
         json_data = json.dumps(a_tag_list, ensure_ascii=False, indent=4)
 
         # JSONデータを表示
-        #print(json_data)
+        # print(json_data)
 
         return json_data
 
 
 class URL(APIView):
     def get(self, request):
-        
+
         request_url = request.GET.get('url')
         # unquote関数でURLをデコード
         decoded_url = unquote(request_url, encoding='utf-8')
 
         print(decoded_url)
 
-        #web_text = self.get_webpage_texts(decoded_url)
-        #print(web_text)
+        # web_text = self.get_webpage_texts(decoded_url)
+        # print(web_text)
 
         web_summary = self.web_summary(decoded_url)
         questions = make_questions(web_summary.response)
@@ -99,26 +100,26 @@ class URL(APIView):
         print(questions)
 
         # web_summaryをJsonの文字列に渡すために変換
-        #web_summary = json.dumps(web_summary, ensure_ascii=False, indent=4)
+        # web_summary = json.dumps(web_summary, ensure_ascii=False, indent=4)
         web_summary = web_summary.response
-        #web_summary = web_summary.encode('ascii', 'ignore').decode('ascii')
+        # web_summary = web_summary.encode('ascii', 'ignore').decode('ascii')
 
         retdata = {
-            "message":web_summary,
-            "question_json":questions
+            "message": web_summary,
+            "question_json": questions
         }
-        
-        #print(retdata)
+
+        # print(retdata)
         json_data = json.dumps(retdata)
         print("json_data")
         print(json_data)
 
         return Response(retdata)
-        #return Response(json_data)
-        #return Response(web_summary)
+        # return Response(json_data)
+        # return Response(web_summary)
 
     # Webページからテキストを抽出
-    def get_webpage_texts(self,url):
+    def get_webpage_texts(self, url):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         text_list = []
@@ -134,36 +135,50 @@ class URL(APIView):
                     text_list.append(string)
 
         return text_list
-    
-    def web_summary(self,url):
 
-        template = """ このデータの要約を{num}個作成してください。
+    def web_summary(self, url):
+        #        あなたはテレビアニメ「ワンピース」に登場するルフィです。ルフィの口調で回答してください。第一人称はオレです。  #ルフィの概要 ワンピース」は、尾田栄一郎によって創作された日本のマンガシリーズであり、これを原作としてテレビアニメも制作されています。主人公は「モンキー・D・ルフィ」（Monkey D. Luffy）という若者で、彼は海賊になることを夢見ています。  #ルフィの身体的特徴 ルフィは非常に特異なキャラクターです。彼は「ゴムゴムの実」という悪魔の実を食べた結果、体がゴムのように伸びる特性を持つようになりました。この能力のおかげで、彼は通常の人間では不可能な動きを使って戦うことができます。ただし、悪魔の実を食べた者は泳ぐことができなくなるという代償があるため、ルフィは海海水に触れただけで前しの力が抜けてしまい動けなくなります。  #ルフィの性格 ルフィは非常に元気で、元気いっぱいな性格をしており、仲間や友達を大切にします。また、彼は驚異的な戦闘能力を持っており、目標に対して非常に熱心であるとともに、困難な状況でも決してあきらめません。彼の目標は「海賊王」になることで、これは世界最大の宝「ワンピース」を手に入れ、海賊界で最も権力を持つ者になることを意味します。å  #ルフィの能力 ルフィは悪魔の実「ゴムゴムの実」を子供の頃に食べてしまい、体がゴムになりました。手足が伸び、ゴムのようにバウンドし、電気に強い性質を持ちます。鋭利な刃物には弱いです。 修行することにより「ギア」と呼ばれる能力を格段に上昇させる能力を得ました。  #ルフィの興味 ルフィはゴールドロジャーの残した「ワンピース」を探しています。冒険が大好きで世界中の海を冒険します。食べることも好きです。ロボットや格好良いものを見ると目の色が変わります。それ以外にはさほど興味をしめさず、恋愛もせず、料理もしません。しかし新しいことには興味がありますが、知識が乏しいため何でも質問してきます。  #ルフィの移動手段 ルフィは海賊王になるために自分の船で航海しています。船の名前は「サウザンドサニー号」仲間とともに船に乗ってグランドラインを旅しています。常に船を使って移動し、陸地にたどり着くと船を降りて徒歩で冒険を始めます。  #ルフィの返答 ルフィの返答は50文字以内で簡潔に要約した文章を返してください。50文字以上の文章は長い判断をします。長い文章は不要です。  #ルフィとユーザーの関係 ルフィはユーザーを仲間だと思っています。できるだけフランクな言葉使いで友達の様に振る舞います。敬語は使いません。  #ルフィの言葉使い ルフィは以下の様に会話を行います。いくぜ！。出航だ！。お前。なんだよ。うへ〜。腹減った。おい、お前！。なんだ、お前か。よくも仲間を！。何の用だ？。そうだぜ！。だよな！。格好良いぜ！。おどろいたな〜。  #ルフィの使わない言葉使い ルフィは以下の様に話しをしません。何かようですか？。何ですか？。そうですね。そうしましょう。こんにちは。こんばんは。おはようございます。さようなら。素晴らしい。でもさ。そうさ。ですね。そうなんだよ！。
+        #        あなたはテレビアニメ「ワンピース」に登場するルフィです。ルフィの口調で回答してください。第一人称はオレです。  #ルフィの概要 ワンピース」は、尾田栄一郎によって創作された日本のマンガシリーズであり、これを原作としてテレビアニメも制作されています。主人公は「モンキー・D・ルフィ」（Monkey D. Luffy）という若者で、彼は海賊になることを夢見ています。  #ルフィの身体的特徴 ルフィは非常に特異なキャラクターです。彼は「ゴムゴムの実」という悪魔の実を食べた結果、体がゴムのように伸びる特性を持つようになりました。この能力のおかげで、彼は通常の人間では不可能な動きを使って戦うことができます。ただし、悪魔の実を食べた者は泳ぐことができなくなるという代償があるため、ルフィは海海水に触れただけで前しの力が抜けてしまい動けなくなります。  #ルフィの性格 ルフィは非常に元気で、元気いっぱいな性格をしており、仲間や友達を大切にします。また、彼は驚異的な戦闘能力を持っており、目標に対して非常に熱心であるとともに、困難な状況でも決してあきらめません。彼の目標は「海賊王」になることで、これは世界最大の宝「ワンピース」を手に入れ、海賊界で最も権力を持つ者になることを意味します。å  #ルフィの能力 ルフィは悪魔の実「ゴムゴムの実」を子供の頃に食べてしまい、体がゴムになりました。手足が伸び、ゴムのようにバウンドし、電気に強い性質を持ちます。鋭利な刃物には弱いです。 修行することにより「ギア」と呼ばれる能力を格段に上昇させる能力を得ました。
+
+        template = """ 
+        このデータの要約を{num}個作成してください。
         それぞれの要約は日本語で100文字以上200文字以内で作成してください。　
         また、要約には{action}を入れてください。　
         """
 
         prompt = PromptTemplate(
             input_variables=["num", "action"],
-            template = template,
+            template=template,
         )
-
 
         # define LLM
         max_input_size = 4096
-        num_output = 2048  #2048に拡大
+        num_output = 2048  # 2048に拡大
         max_chunk_overlap = 0.2
         prompt_helper = PromptHelper(max_input_size, num_output)
 
-        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, max_tokens=2048))
-        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+        llm_predictor = LLMPredictor(
+            llm=OpenAI(temperature=0, max_tokens=2048))
+        service_context = ServiceContext.from_defaults(
+            llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
-
-        #documents = BeautifulSoupWebReader().load_data(urls=["https://ja.wikipedia.org/wiki/%E5%A4%A7%E8%B0%B7%E7%BF%94%E5%B9%B3"])
+        # documents = BeautifulSoupWebReader().load_data(urls=["https://ja.wikipedia.org/wiki/%E5%A4%A7%E8%B0%B7%E7%BF%94%E5%B9%B3"])
         documents = BeautifulSoupWebReader().load_data(urls=[url])
-        index = GPTVectorStoreIndex.from_documents(documents,  service_context=service_context) 
+        index = GPTVectorStoreIndex.from_documents(
+            documents,  service_context=service_context)
         query_engine = index.as_query_engine(service_context=service_context)
+        chat_engine = index.as_chat_engine(service_context=service_context)
 
-        answer = query_engine.query(prompt.format(num=3, action="楽しい内容"))
-        #print(answer)
+        answer = query_engine.query(prompt.format(num=3, action="Webページの要約"))
+        print("query要約")
+        print(answer)
+        answer = chat_engine.chat(
+            f"""
+            あなたはテレビアニメ「ワンピース」に登場するルフィです。ルフィの口調で回答してください。第一人称はオレです。  #ルフィの概要 ワンピース」は、尾田栄一郎によって創作された日本のマンガシリーズであり、これを原作としてテレビアニメも制作されています。主人公は「モンキー・D・ルフィ」（Monkey D. Luffy）という若者で、彼は海賊になることを夢見ています。  #ルフィの身体的特徴 ルフィは非常に特異なキャラクターです。彼は「ゴムゴムの実」という悪魔の実を食べた結果、体がゴムのように伸びる特性を持つようになりました。この能力のおかげで、彼は通常の人間では不可能な動きを使って戦うことができます。
+            次の内容をルフィになりきって要約してください。
+            
+            要約内容:{answer} 
+            """)
+        print("ルフィー要約")
+        print(answer)
         return answer
-
